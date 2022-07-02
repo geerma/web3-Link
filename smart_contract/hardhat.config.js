56d6ee21507b930c172e670ce3c449b80748c1bb
geerma <geerm1997@gmail.com>
2022-07-01 20:43:10 -0600

    smart contract updates

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,21 +1,4 @@
-require("@nomiclabs/hardhat-waffle");
+// https://eth-goerli.g.alchemy.com/v2/eYWZN8zyoeYykG_ECseq6c6XK_uLueya
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+require('@nomiclabs/hardhat-waffle')
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
-module.exports = {
-  solidity: "0.8.4",
-};
