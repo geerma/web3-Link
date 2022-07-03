28f9cf9ad71e8fec4a738edb7a6ef565b69ad756
geerma <geerm1997@gmail.com>
2022-07-03 16:54:44 -0600

    hardhat config

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,4 +1,11 @@
-// https://eth-goerli.g.alchemy.com/v2/eYWZN8zyoeYykG_ECseq6c6XK_uLueya
-
 require('@nomiclabs/hardhat-waffle')
 
+module.exports = {
+    solidity: '0.8.0',
+    networks: {
+        goerli: {
+            url: 'https://eth-goerli.g.alchemy.com/v2/eYWZN8zyoeYykG_ECseq6c6XK_uLueya',
+            accounts: [ '' ]
+        }
+    }
+}
